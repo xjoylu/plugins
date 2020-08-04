ec87ecd97665be8721279ac82f396408f0e42181
jiix <48614618+jiix@users.noreply.github.com>
2020-08-04 23:36:40 +0800

    add

---
+++ plugins/huux_theme_simple/install.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+	Xiuno BBS 4.0 大白主题
+*/
+
+!defined('DEBUG') AND exit('Forbidden');
+
+
+
+
+
+
+
+
+?>
---
+++ plugins/huux_theme_simple/unstall.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+	Xiuno BBS 4.0 大白主题
+*/
+
+!defined('DEBUG') AND exit('Forbidden');
+
+
+?>
